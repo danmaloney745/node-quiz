d34e297ae7318d6c21b42167654b5d3241428750
G00303381 <danielmaloneygti@gmail.com>
2017-04-14 19:15:13 +0100

    Added prev and next button functionality

--- temp.js
+++ temp.js
@@ -13,48 +13,30 @@ $(() => {
 
         displayQuestion(theData, questionCounter);
 
-        /**
-         * Function that is called when the user clicks the next button
-         */
         $('#next').on('click', function (e){
             e.preventDefault();
-            removeAnswers(answersArray);
             questionCounter++;
             displayQuestion(theData, questionCounter);
         });
 
-        /**
-         * Function that is called when the user cliecks the previous button
-         */
         $('#prev').on('click', function (e){
             e.preventDefault();
-            removeAnswers(answersArray);
             questionCounter--;
             if(questionCounter >= 0){
                 displayQuestion(theData, questionCounter);
             }
         });
 
-        /**
-         * Function to print the question to the DOM elements, then populate the answer array.
-         * @param {Object} data 
-         * @param {Integer} questionCounter 
-         */
         function displayQuestion(data, questionCounter) {
             $("#quizCategory").text(`Category: ${data[questionCounter].category}`);
             $("#question").text(`Question: ${data[questionCounter].question}`);
             populateAnswers(data, questionCounter);
         }
 
-        /**
-         * Function that populates the answers array with the correct and wrong answers.
-         * @param {*} data 
-         * @param {*} questionCounter 
-         */
         function populateAnswers(data, questionCounter){
             //push the correct answer to the array
             answersArray.push(data[questionCounter].correct_answer);
-
+            
             //loop through answer array defined in JSON object
             for(let i = 0; i < data[questionCounter].incorrect_answers.length; i++){
                 //push the wrong answers to the array
@@ -66,29 +48,13 @@ $(() => {
             loopArray(answersArray);
         }
 
-        /**
-         * Function that removes the contents of the answers array
-         * @param {Array} answers 
-         */
-        function removeAnswers(answers){
-            array.splice(0, answers.length)
-        }
-
-        /**
-         * Function to loop through the answers
-         * @param {Array} answers 
-         */
-        function loopArray(answers){
-            for(let i = 0; i < answers.length; i++){
-                populateButtons(i, answers[i]);
+        function loopArray(params){
+            for(i = 0; i < params.length; i++){
+                console.log(params[i]);
+                populateButtons(i, params[i]);
             }
         }
 
-        /**
-         * Function that populates the DOM with the correct and wrong answers
-         * @param {Integer} position 
-         * @param {String} answer 
-         */
         function populateButtons(position, answer) {
             $(`#answer${position}`).text(`${answer}`);
         }
